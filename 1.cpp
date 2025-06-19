a84b5d838b62a055ea46410008eb3973f645140f
snb2005 <iit2023502@iiita.ac.in>
2025-06-19 17:42:28 +0530

    test commit

---
+++ 1.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+#define ll long long
+
+const int mod=1e9+7;
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin>>t;
+    while(t--){
+        ;
+    }
+}
